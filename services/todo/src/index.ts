2e5212c8df036b2d006fbd688698869ece64b0bf
Tamino Martinius <dev@zaku.eu>
2019-02-06 07:25:46 +0100

    Project entry is not at index.js

--- services/todo/src/index.ts
+++ services/todo/src/index.ts
@@ -1,24 +1,14 @@
-import {AccountMicroservice} from './application';
-import {RestServer, RestBindings} from '@loopback/rest';
+import {TodoMicroservice} from './application';
+import {ApplicationConfig} from '@loopback/core';
 
-export async function main() {
-  const app = new AccountMicroservice();
-  try {
-    await app.boot();
-    await app.start();
-    console.log('Application Info:', await info());
-  } catch (err) {
-    console.error(`Cannot start the app ${err}`);
-    process.exit(1);
-  }
+export {TodoMicroservice};
 
-  async function info() {
-    const rest = await app.getServer<RestServer>(RestServer);
-    const port = await rest.get<Number>(RestBindings.PORT);
-    return {
-      appName: 'account',
-      uptime: Date.now() - app._startTime.getTime(),
-      url: `http://127.0.0.1:${port}`,
-    };
-  }
+export async function main(options: ApplicationConfig = {}) {
+  const app = new TodoMicroservice(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`TodoMicroservice is running at ${url}`);
+  return app;
 }
