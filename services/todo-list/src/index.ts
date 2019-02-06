e8b3236a895dbefd7c6a224cdfcfca1b9025bb04
Tamino Martinius <dev@zaku.eu>
2019-02-06 18:49:41 +0100

    Change startup files from transation to todo list

--- services/todo-list/src/index.ts
+++ services/todo-list/src/index.ts
@@ -1,24 +1,14 @@
-import {TranscationMicroservice} from './application';
-import {RestServer, RestBindings} from '@loopback/rest';
+import {TodoListMicroservice} from './application';
+import {ApplicationConfig} from '@loopback/core';
 
-export async function main() {
-  const app = new TranscationMicroservice();
-  try {
-    await app.boot();
-    await app.start();
-    console.log('Application Info:', await info());
-  } catch (err) {
-    console.error(`Cannot start the app ${err}`);
-    process.exit(1);
-  }
+export {TodoListMicroservice};
 
-  async function info() {
-    const rest = await app.getServer<RestServer>(RestServer);
-    const port = await rest.get<Number>(RestBindings.PORT);
-    return {
-      appName: 'transcation',
-      uptime: Date.now() - app._startTime.getTime(),
-      url: `http://127.0.0.1:${port}`,
-    };
-  }
+export async function main(options: ApplicationConfig = {}) {
+  const app = new TodoListMicroservice(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`TodoListMicroservice is running at ${url}`);
+  return app;
 }
