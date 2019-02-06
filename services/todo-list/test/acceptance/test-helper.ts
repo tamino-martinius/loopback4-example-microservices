a68ad88cbec0fbc44d5d46cbbadf4a272b8289fb
Tamino Martinius <dev@zaku.eu>
2019-02-06 18:48:59 +0100

    Change tests from transation to todo list

---
+++ services/todo-list/test/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {TodoListMicroservice} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new TodoListMicroservice({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: TodoListMicroservice;
+  client: Client;
+}
